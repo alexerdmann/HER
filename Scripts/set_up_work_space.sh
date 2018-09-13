fc57fa0fc5069a02b763b9b49ab4e0c8a4e14734
alexerdmann <32928974+alexerdmann@users.noreply.github.com>
2018-09-13 09:25:22 -0400

    Add files via upload

--- Scripts/set_up_work_space.sh
+++ Scripts/set_up_work_space.sh
@@ -1,9 +1,17 @@
 name_of_project=$1
 
 mkdir $name_of_project
-for dir in Data Models Scripts Results; do cp -r $dir $name_of_project/.; done
+for dir in Data Scripts; do cp -r $dir $name_of_project/.; done
 cd $name_of_project
 rm Data/Original/*
+mkdir -p Models/CRF
+mkdir Models/RankedSents
+mkdir Results
+mkdir Data/Preprocessed
+mkdir Data/Tokenized
+mkdir Data/Prepared
+mkdir Data/Splits
+mkdir Data/Features
 
 echo " "
 echo "Please cp all raw texts in your corpus to Data/Original/."
