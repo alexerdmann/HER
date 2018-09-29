c931df632738ccc86dad0cc5a29b884714f84584
alexerdmann <32928974+alexerdmann@users.noreply.github.com>
2018-09-29 13:08:47 -0400

    Add files via upload

--- Scripts/tag_get_final_results.sh
+++ Scripts/tag_get_final_results.sh
@@ -17,11 +17,12 @@ crfsuite tag -m Models/CRF/best_seed.cls $unannotated.fts > $predictions
 awk 'FNR==NR{a[NR]=$1;next}{$1=a[FNR]}1' $predictions $unannotated > $predictions.aligned
 ### build the full annotated corpus
 cat $seed $alwaysTrain $predictions.aligned > $final_corpus
-echo "FINAL CORPUS (MANUAL ANNOTATIONS + AUTOMATICALLY PREDICTED ANNOTATIONS):"
+echo
+echo "Full Annotated Corpus (Manual + Automatic Predictions)"
 echo $final_corpus
 echo
 ### get the final list of named entities
 python Scripts/get_NE_list.py Results/fullCorpus.final.txt > $final_list
-echo "FINAL NAMED ENTITY LIST:"
+echo "Full List of Entities (Manually or Automatically Identified)"
 echo $final_list
 echo
